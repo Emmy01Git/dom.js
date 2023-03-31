c7a476b3437c514233c674d59a122a795e2f985d
Emmy01Git <amitsku.1994@gmail.com>
2023-03-31 05:59:33 +0530

    updated

---
+++ dom.js
@@ -0,0 +1,20 @@
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
+
+//GETELEMENTSBYCLASSNAME
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+//items[2].textContent = "Changed Name";
+//items[2].style.fontWeight = 'bold';
+ 
+for(var i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
+
+var item = document.querySelector('.list-group-item');
+//item.style.color = 'green';
+
+for(var i = 0; i < items.length; i++){
+    items[i].style.color = 'yellow';
+}
